c5276320fe5321d9c5095aae42525038c349e39c
denchiks <pereligin.3004@gmail.com>
2024-07-19 17:20:33 +0300

    versio3

--- test1/Main.java
+++ test1/Main.java
@@ -5,8 +5,9 @@ package test1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Version 1.0");
-        System.out.println("Version 2.0");
+        System.out.println("Version 1.5");
         System.out.println("Version 3.0");
+
 
 
 
