c2cda67446cef6bd03fec7ddf3d2147ca6afcb85
denchiks <pereligin.3004@gmail.com>
2024-07-19 17:19:19 +0300

    version2

---
+++ test1/Main.java
@@ -0,0 +1,14 @@
+package test1;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Version 1.0");
+        System.out.println("Version 2.0");
+        System.out.println("Version 3.0");
+
+
+
+    }
+}
